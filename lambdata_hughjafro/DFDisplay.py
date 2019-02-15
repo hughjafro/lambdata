9cd3ded0ae5eca8e255210ad1d7c6901a094ceeb
hughjafro <cmtolbert1@gmail.com>
2019-02-14 23:22:28 -0800

    Pep8 compliant

--- lambdata_hughjafro/DFDisplay.py
+++ lambdata_hughjafro/DFDisplay.py
@@ -4,12 +4,11 @@ The DFDispaly class is a useful display helper
 '''
 
 Class DFDispaly(object):
-    def __init__(self)
-    #Initialize the function
+    def __init__(self)  # Initialize the function
     self.randrange()
     self.unlimited_columns()
     self.unlimited_rows()
-    
+
     def randrange(n, vmin, vmax):
         '''
         Helper function to make an array of random numbers having shape (n, )
@@ -20,9 +19,11 @@ Class DFDispaly(object):
     def unlimited_columns(self):
             '''
             Helper function to display unlimited columns.
-            When dealing with dataframes with many columns, this function will allow all columns to be viewed.
+            When dealing with dataframes with many columns, this function
+            will allow all columns to be viewed.
 
-            Using transpose or df.T, is a useful operation when a viewing a dataframe with many features. 
+            Using transpose or df.T, is a useful operation when
+            viewing a dataframe with many features.
             '''
             print("pd.set_option('display.max_columns', None)")
 
